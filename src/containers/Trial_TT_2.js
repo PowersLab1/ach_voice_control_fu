5ac70231e9ba6352db5add5108e5e7ac083a4458
PowersLab1 <powerslab3@gmail.com>
2025-06-20 20:18:51 -0400

    Update Trial_TT_2.js
    redundant logic

--- src/containers/Trial_TT_2.js
+++ src/containers/Trial_TT_2.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-
 import { Redirect } from "react-router-dom";
 import Trial from "./Trial";
-
 import { setComponentData } from "../store";
 import { create_blocks_singleton } from "../lib/tt_blocks";
-
 var _ = require("lodash");
+const config = require("../config");
 
 const TRIAL_NUM = 2;
 const BLOCK_START = 3;
@@ -15,18 +13,26 @@ const BLOCK_END = 6; // not inclusive
 class Trial_TT_2 extends Component {
   constructor(props) {
     super(props);
-
-    // initial states
     this.startTimestamp = new Date().getTime();
+
+    // Use explicit arguments to guarantee block creation in debug/dev
+    const decibels = _.flatten(
+      _.slice(create_blocks_singleton(25, 50, 75), BLOCK_START, BLOCK_END),
+    );
+    console.log("Trial_TT_2 decibels (before setState):", decibels);
     this.state = {
-      decibels: _.flatten(
-        _.slice(create_blocks_singleton(), BLOCK_START, BLOCK_END),
-      ),
+      decibels,
     };
   }
 
   trialCompleteRenderer = (decibels, response) => {
-    return <Redirect to="/Break2" />;
+    console.log("Trial_TT_2 complete, redirecting to Break2");
+    // If debugging, then we're done here
+    if (config.debug) {
+      return <Redirect to="/Break2" />;
+    } else {
+      return <Redirect to="/Break2" />;
+    }
   };
 
   dataHandler = (
@@ -37,6 +43,7 @@ class Trial_TT_2 extends Component {
     ratingsRaw,
     timestamps,
   ) => {
+    console.log("Trial_TT_2 data handler called");
     setComponentData(
       TRIAL_NUM,
       decibels,
@@ -50,9 +57,11 @@ class Trial_TT_2 extends Component {
   };
 
   render() {
-    // Something went wrong and we don't have contrast values from Quest.
+    // If decibels is empty, show error
     if (_.isEmpty(this.state.decibels)) {
-      return <Redirect to="/Error" />;
+      return <div>Error: No decibels</div>;
+      // Or, if you want to redirect:
+      // return <Redirect to="/Error" />;
     }
 
     return (
@@ -62,10 +71,10 @@ class Trial_TT_2 extends Component {
         trialCompleteRenderer={this.trialCompleteRenderer}
         dataHandler={this.dataHandler}
         blockIdx={TRIAL_NUM}
-        constantCrossColor="default" // or "gray", "black", etc.
+        constantCrossColor="default"
       />
     );
-  } // end render
-} // end class
+  }
+}
 
 export default Trial_TT_2;
